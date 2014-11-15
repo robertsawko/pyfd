babea52ff5e300655308e24b08f1373015b72fa2
Robert Sawko <robertsawko@gmail.com>
2014-11-15 14:42:02 +0000

    Change tests to significant figures

--- friction_factor_calc.py
+++ friction_factor_calc.py
@@ -9,15 +9,24 @@ def reynolds_number(U, L, nu):
 def colebrook_white_equation(f, Re, Dh, epsilon=0):
     return (
         1/np.sqrt(f)
-        + 2 * np.log10(2.51 / (Re*np.sqrt(f) + epsilon/(3.7 * Dh))))
+        + 2 * np.log10(2.51 / (Re * np.sqrt(f) + epsilon/(3.7 * Dh))))
 
 
 def friction_factor(Re, Dh):
-    def friction_eq(f):
-        return colebrook_white_equation(f, Re, Dh)
+    if Re > 2100:
+        def friction_eq(f):
+            return colebrook_white_equation(f, Re, Dh)
+        f = fsolve(friction_eq, 64 / Re)
+        return f
+    else:
+        return 64. / Re
 
-    f = fsolve(friction_eq, 64 / Re)
-    return f
+
+def pressure_drop(U, L, D, rho, nu):
+    Re = reynolds_number(U, D, nu)
+    f = friction_factor(Re, D)
+    return rho * f * L / D * (U**2 / 2)
+
 
 # Geometry specification
 pipe_diameter = 48 * 0.0254
@@ -31,16 +40,6 @@ oil_density = 871.5222
 oil_inlet_velocity = 0.3  # m/s
 pig_velocity = 0.0  # m/s
 
-Re_pipe = reynolds_number(
-    oil_inlet_velocity, pipe_diameter, oil_kinematic_viscosity)
-
 orifice_velocity = (
     (oil_inlet_velocity - pig_velocity)
     * (pipe_diameter / orifice_diameter)**2)
-
-Re_orifice = reynolds_number(
-    orifice_velocity, orifice_diameter, oil_kinematic_viscosity)
-
-print Re_pipe
-print Re_orifice
-print friction_factor(Re_pipe, pipe_diameter)
--- test_friction_factor_calc.py
+++ test_friction_factor_calc.py
@@ -1,16 +1,39 @@
-from friction_factor_calc import friction_factor, reynolds_number
-from nose.tools import assert_almost_equal
+from friction_factor_calc import (
+    friction_factor, reynolds_number, pressure_drop)
+from numpy.testing import assert_approx_equal
+
+
+#  Friction factor for laminar calculations are 64/Re
+#  Applies only for Re lower than 2100
+def test_laminar():
+    for Re in [1, 10, 100, 1000]:
+        assert_approx_equal(
+            friction_factor(Re, 1),
+            64./Re,
+            significant=2
+        )
+
+
+D = 48 * 0.0254
+U = 0.3
+nu = 1.9484e-05
+rho = 871.5222
+L = 50
 
 
 #  Results based on
 #  http://www.efunda.com/formulae/fluids/calc_pipe_friction.cfm#calc
-def test_vs_efunda():
-    D = 48 * 0.0254
-    U = 0.3
-    nu = 1.9484e-05
-    print U*D/nu
-    assert_almost_equal(
+def test_f_vs_efunda():
+    assert_approx_equal(
         friction_factor(reynolds_number(U, D, nu), D),
         0.0263,
-        delta=1e-4
+        significant=2
+    )
+
+
+def test_dp_vs_efunda():
+    assert_approx_equal(
+        pressure_drop(U, L, D, rho, nu),
+        0.0422 * 1000,  # 0.0422 kPa
+        significant=2
     )
