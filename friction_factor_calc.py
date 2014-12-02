51825c20b6571837b44edb6d6b979125dd9d8d3f
Robert Sawko <r.sawko@cranfield.ac.uk>
2014-12-02 23:22:23 +0000

    Document Colebrook White equation

--- friction_factor_calc.py
+++ friction_factor_calc.py
@@ -7,16 +7,30 @@ def reynolds_number(U, L, nu):
 
 
 def colebrook_white_equation(f, Re, Dh, epsilon=0):
-    return (
-        1/np.sqrt(f)
-        + 2 * np.log10(2.51 / (Re * np.sqrt(f) + epsilon/(3.7 * Dh))))
+    """
+    colebrook_white_equation description
+
+    @type f: float
+    @param f: friction factor value
+
+    @type epsilon: float
+    @param epsilon: dimensional rougness parameter
+
+    @type Re: float
+    @param Re: Reynolds number
+
+    @type Dh: float
+    @param Dh: Hydraulic diameter
+    """
+    return 1 / np.sqrt(f) + \
+        2 * np.log10(2.51 / (Re * np.sqrt(f) + epsilon / (3.7 * Dh)))
 
 
 def friction_factor(Re, Dh):
     if Re > 2100:
         def friction_eq(f):
             return colebrook_white_equation(f, Re, Dh)
-        f = fsolve(friction_eq, 64 / Re)
+        f = fsolve(friction_eq, 64. / Re)
         return f
     else:
         return 64. / Re
