55d3134faef8ba386a04250ffe147288808cff77
Robert Sawko <r.sawko@cranfield.ac.uk>
2014-12-03 10:22:54 +0000

    Correct analytical weights and abscissas
    I calculated the correct xi and w with Maxima.

--- pbe/test_wheeler.py
+++ pbe/test_wheeler.py
@@ -7,14 +7,12 @@ from numpy.testing import assert_almost_equal
 #  Applies only for Re lower than 2100
 def test_book_ex3_2():
     moments = np.array([1, 0, 1, 0, 3, 0, 15, 0])
-    abscissas = np.array([-2.3344, -0.7420, 0.7420, 2.3344])
-    weights = np.array([0.0459, 0.4541, 0.4541, 0.0459])
+    xi1 = np.sqrt(np.sqrt(6) + 3)  # Taken from Maxima
+    xi2 = np.sqrt(3 - np.sqrt(6))
+    w1 = 1/(2*np.sqrt(6) + 2**(3./2)*np.sqrt(3) + 12)
+    w2 = 1/(12 - 2*np.sqrt(6) - 2**(3./2)*np.sqrt(3))
+    abscissas = np.array([-xi1, -xi2, xi2, xi1])
+    weights = np.array([w1, w2, w2, w1])
     [xi, w] = wheeler_inversion(moments)
-    assert_almost_equal(
-        np.max(np.abs(xi-abscissas)), 0,
-        decimal=4
-    )
-    assert_almost_equal(
-        np.max(np.abs(w-weights)), 0,
-        decimal=4
-    )
+    assert_almost_equal(np.max(np.abs(xi-abscissas)), 0)
+    assert_almost_equal(np.max(np.abs(w-weights)), 0)
