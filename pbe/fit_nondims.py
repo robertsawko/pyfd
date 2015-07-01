a1993ebdf0f4f61ea83cb85f010ec16a7b50853a
s200292 <marcin.traczyk.88@gmail.com>
2015-07-01 13:43:53 +0100

    ADD parameter fitting for Ci(Re, St, Ca)

---
+++ pbe/fit_nondims.py
@@ -0,0 +1,85 @@
+import scipy as sp
+import numpy as np
+from scipy.optimize import minimize, fmin, basinhopping, brute
+import matplotlib.pyplot as plt
+from itertools import cycle
+from scipy.optimize import curve_fit
+
+I = 1
+skipGalinat = False
+skipSimmons = True
+
+
+def xAxis(x, aRe, aSt, aCa, C1, C2):
+  Re = x[0]
+  St = x[1]
+  Ca = x[2]
+  # works for I=3
+  #return Re ** aRe / St ** aSt / Ca ** aCa
+
+  # works for I=2
+  #return np.log(Re ** aRe) * St ** aSt * Ca ** aCa
+
+  # works for I=1
+  return np.log(St ** aSt)
+
+def dependency(x, A, B, aRe, aSt, aCa, C1, C2):
+  x = xAxis(x, aRe, aSt, aCa, C1, C2)
+  return A * x + B
+
+def getData():
+    C = np.genfromtxt('validationData/coeffs_nondims.txt')
+    if(skipSimmons):
+      C = np.delete(C, 4, 0)
+    C = C.T
+    c = []
+    if(skipGalinat):
+      Re = C[4][4:]
+      St = C[5][4:]
+      Ca = C[6][4:]
+      iMin = 4
+      for i in range(4):
+          c.append(C[i][4:])
+    else:
+      # Reynolds number in the orifice is twice as big as Re_pipe
+      Re = C[4][1:4] * 2.0
+      # St is 8 times as big
+      St = C[5][1:4] * 8.0
+      # Ca is 4 times as big
+      Ca = C[6][1:4] * 4.0
+      Re = np.append(Re, C[4][4:])
+      St = np.append(St, C[5][4:])
+      Ca = np.append(Ca, C[6][4:])
+      for i in range(4):
+          c.append(C[i][1:])
+
+
+    return Re, St, Ca, np.array(c)
+
+Re, St, Ca, C = getData()
+C[2] *= 1e14
+C[3] /= 1e12
+X = [Re, St, Ca]
+popt, pcov = curve_fit(dependency, X, C[I])
+print('A: ', popt[0])
+print('B: ', popt[1])
+print('aRe: ', popt[2])
+print('aSt: ', popt[3])
+print('aCa: ', popt[4])
+print('C1: ', popt[5])
+print('C2: ', popt[6])
+
+x = xAxis(X, *popt[2:])
+y = dependency(X, *popt)
+x_cont = np.linspace(min(x) / 2.0, max(x) * 2.0, 1000)
+
+fig = plt.figure()
+plt.plot(
+    x, C[I], '+',
+    linewidth=2, label="correlation")
+plt.plot(
+    x_cont, popt[0] * x_cont + popt[1],
+    linewidth=2, label="correlation")
+plt.xscale('log')
+#plt.yscale('log')
+plt.show()
--- pbe/plot_nondims.py
+++ pbe/plot_nondims.py
@@ -4,9 +4,9 @@ from scipy.optimize import minimize, fmin, basinhopping, brute
 import matplotlib.pyplot as plt
 from itertools import cycle
 
-aRe = -2
-aSt = 1
-aCa = 1
+aRe = 1
+aSt = 2
+aCa = 0
 I = 2
 skipGalinat = True
 
@@ -52,20 +52,34 @@ def coulaloglou():
 
 def angeli():
     C = np.genfromtxt('validationData/coeffs_nondims.txt').T
-    Re = C[4][19:]
-    St = C[5][19:]
-    Ca = C[6][19:]
+    Re = C[4][19:24]
+    St = C[5][19:24]
+    Ca = C[6][19:24]
     c = []
     for i in range(4):
-        c.append(C[i][19:])
+        c.append(C[i][19:24])
 
     return Re, St, Ca, c
+
+
+def karabelas():
+    C = np.genfromtxt('validationData/coeffs_nondims.txt').T
+    Re = C[4][24:]
+    St = C[5][24:]
+    Ca = C[6][24:]
+    c = []
+    for i in range(4):
+      c.append(C[i][24:])
+
+    return Re, St, Ca, c
+
 
 nondims = dict()
 nondims['simmons'] = (simmons())
 nondims['galinat'] = (galinat())
 nondims['coulaloglou'] = (coulaloglou())
 nondims['angeli'] = (angeli())
+nondims['karabelas'] = (karabelas())
 
 fig = plt.figure()
 ax = fig.gca()
@@ -80,6 +94,7 @@ for name in nondims:
     Ca = data[2]
     C = data[3]
     x = Re ** aRe * St ** aSt * Ca ** aCa
+    #x = Re / St ** 2
 
     if skipGalinat and name == 'galinat':
         continue
@@ -94,19 +109,19 @@ x = np.linspace(xMin, xMax)
 # for C4 x = Re / St
 #y = 3.0e08 * x
 #for C3; x = St * Ca / Re ** 2
-y = 1.0e-07 * x ** 0.5
+#y = 1.0e-07 * x ** 0.5
 # for C2; x = St
-#y = np.exp(x / 14.0) / 40.0
+y = np.exp(x / 14.0) / 40.0
 #for C1; x = St
 #y = np.exp(x / 14.0) / 400.0
-ax.plot(
-    x, y,
-    linewidth=2, label="correlation")
+#ax.plot(
+    #x, y,
+    #linewidth=2, label="correlation")
 
 ax.legend(loc='best', shadow=True)
 ax.grid()
 ax.set_xscale('log')
-ax.set_yscale('log')
+#ax.set_yscale('log')
 ax.set_xlabel('St * Ca / Re^2')
 ax.set_ylabel('C' + repr(I + 1))
 plt.savefig('validationData/plots/C' + repr(I + 1) + '.pdf')
