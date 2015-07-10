7a4159e22f831fc7fcd07701740efcd9ae797550
Marcin Traczyk <marcin.traczyk.88@gmail.com>
2015-07-10 17:12:31 +0100

    MOD updated parameter fitting

--- pbe/fit_nondims.py
+++ pbe/fit_nondims.py
@@ -4,31 +4,45 @@ from scipy.optimize import minimize, fmin, basinhopping, brute
 import matplotlib.pyplot as plt
 from itertools import cycle
 from scipy.optimize import curve_fit
+from scipy.special import erf, erfc
 
 I = 2
 skipGalinat = True
-skipSimmons = True
+skipSimmons = False
 
 
-def xAxis(x, aRe, aSt, aCa, C1, C2):
+def xAxis(x, aRe, aSt, aCa, aWe, C1, C2):
     Re = x[0]
     St = x[1]
     Ca = x[2]
-    # works for I=3 and I=0
-    #return Re ** aRe / St ** aSt / Ca ** aCa
+    We = x[3]
+    # I=0
+    # increase with We, possibly with Ca
+    # decrease with Re
+    # no dependency on St
+    #return Re ** aRe * We ** aWe
 
-    # works for I=2
-    #return St ** aSt / Re ** aRe * Ca ** aCa
-    #return (St ** aSt + C1)
-    #return (Re ** (aRe - aSt * St / 10) + C1)
-    return St ** aSt / Re ** aRe / Ca ** aCa
+    # I=1
+    # increase with We
+    # decrease with Re
+    # no dependency on St, Ca
+    #return We ** aWe
 
-  # works for I=1
-  #return Re ** aRe
+    # I=2;
+    # increase with We (slowly), St
+    # decrease with
+    # no dependency on Re, Ca
+    return St ** aSt * (Ca ** aCa * C1 + C2)
+
+    # I=3
+    # increase with Re(fast),
+    # decrease with St, We
+    # no dependency on Ca
+    #return Re ** aRe / St ** aSt / We ** aWe
 
 
-def dependency(x, A, B, aRe, aSt, aCa, C1, C2):
-    x = xAxis(x, aRe, aSt, aCa, C1, C2)
+def dependency(X, A, B, aRe, aSt, aCa, aWe, C1, C2):
+    x = xAxis(X, aRe, aSt, aCa, aWe, C1, C2)
     return A * x + B
 
 
@@ -42,6 +56,7 @@ def getData():
         Re = C[4][4:]
         St = C[5][4:]
         Ca = C[6][4:]
+        We = C[7][4:]
         iMin = 4
         for i in range(4):
             c.append(C[i][4:])
@@ -52,18 +67,20 @@ def getData():
         St = C[5][1:4] * 8.0
         # Ca is 4 times as big
         Ca = C[6][1:4] * 4.0
+        We = C[7][4:]
         Re = np.append(Re, C[4][4:])
         St = np.append(St, C[5][4:])
         Ca = np.append(Ca, C[6][4:])
+        We = np.append(We, C[7][4:])
         for i in range(4):
             c.append(C[i][1:])
 
-    return Re, St, Ca, np.array(c)
+    return Re, St, Ca, We, np.array(c)
 
-Re, St, Ca, C = getData()
+Re, St, Ca, We, C = getData()
 #C[2] *= 1e14
 #C[3] /= 1e12
-X = [Re, St, Ca]
+X = [Re, St, Ca, We]
 popt, pcov = curve_fit(dependency, X, C[I])
 #print pcov
 #print('Error is ',  np.sqrt(np.diag(pcov)))
@@ -72,12 +89,13 @@ print('B: ', popt[1])
 print('aRe: ', popt[2])
 print('aSt: ', popt[3])
 print('aCa: ', popt[4])
-print('C1: ', popt[5])
-print('C2: ', popt[6])
+print('aWe: ', popt[5])
+print('C1: ', popt[6])
+print('C2: ', popt[7])
 
 x = xAxis(X, *popt[2:])
 y = dependency(X, *popt)
-x_cont = np.linspace(min(x) / 2.0, max(x) * 2.0, 1000)
+x_cont = np.linspace(min(x), max(x), 1000)
 
 fig = plt.figure()
 plt.plot(
@@ -86,6 +104,15 @@ plt.plot(
 plt.plot(
     x_cont, popt[0] * x_cont + popt[1],
     linewidth=2, label="correlation")
-plt.xscale('log')
-plt.yscale('log')
+#plt.xscale('log')
+#plt.yscale('log')
+#plt.show()
+
+fig = plt.figure()
+plt.plot(
+    x, C[I], '+',
+    linewidth=2, label="correlation")
+plt.loglog(
+    x_cont, popt[0] * x_cont + popt[1],
+    linewidth=2, label="correlation")
 plt.show()
