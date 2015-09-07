61e909bfb8c20eaa99e3bf089d8676f158850cd8
Robert Sawko <robertsawko@gmail.com>
2015-09-07 20:33:15 +0100

    Fix the comparison graph

--- pbe/kernel_identification/plot_comparison.py
+++ pbe/kernel_identification/plot_comparison.py
@@ -1,48 +1,28 @@
-import scipy as sp
 import numpy as np
-from scipy.optimize import minimize, fmin, basinhopping, brute
-import matplotlib.pyplot as plt
-from itertools import cycle
+from aux import plt, set_plt_params
 
+set_plt_params()
 fig = plt.figure()
 ax = fig.gca()
-markers = cycle(['o', 's', 'v', '*', '.', ','])
+
 
 dExp = np.loadtxt('validationData/comparison/dExp.txt') * 1e03
 dNum = np.loadtxt('validationData/comparison/dNum.txt') * 1e03
+x = np.linspace(0, 10)
 
-#ax.legend(loc='best', shadow=True)
-ax.grid()
+ax.plot(dExp, dNum, 'ok')
 
-print dNum
-ax.plot(
-    dExp, dNum, '+', marker=next(markers), color='black',
-    linewidth=2)
+line, = ax.plot(x, 1.1 * x, '--k', linewidth=0.5)
+line, = ax.plot(x, 0.9 * x, '--k', linewidth=0.5)
 
-line, = ax.plot(
-    dExp, 1.1 * dExp, linewidth=1, color='black', linestyle=':')
-line.set_dashes([6, 20])
-line, = ax.plot(
-    dExp, 0.9 * dExp, linewidth=1, color='black', linestyle=':')
-line.set_dashes([6, 20])
+line, = ax.plot(x, 1.2 * x, ':k', linewidth=0.25)
+line, = ax.plot(x, 0.8 * x, ':k', linewidth=0.25)
 
-line, = ax.plot(
-    dExp, 1.2 * dExp, linewidth=1, color='black', linestyle='--')
-line.set_dashes([1, 10])
-line, = ax.plot(
-    dExp, 0.8 * dExp, linewidth=1, color='black', linestyle='--')
-line.set_dashes([1, 10])
-
-line, = ax.plot(
-    dExp, dExp, linewidth=1, color='black')
+line, = ax.plot(dExp, dExp, 'k')
 
 ax.set_xlim(0, 1.5)
 ax.set_ylim(0, 1.5)
-#ax.set_xscale('log')
-#ax.set_yscale('log')
 ax.set_ylabel('d (numerical) [mm]')
 ax.set_xlabel('d (experimental) [mm]')
-plt.savefig('validationData/plots/comparison.pdf')
-plt.show()
-
-print dExp.shape
+fig.patch.set_alpha(0)
+plt.savefig('validationData/plots/comparison.pgf', bbox_inches='tight')
