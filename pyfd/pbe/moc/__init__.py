b42b891f626671dda934edc93791c8101f68e011
s200292 <marcin.traczyk.88@gmail.com>
2015-11-10 11:35:28 +0000

    ADD simmons&azzopardi class drafted

--- pyfd/pbe/moc/__init__.py
+++ pyfd/pbe/moc/__init__.py
@@ -1,4 +1,5 @@
 from moc import MOCSolution
 from case_class import beta, CaseSolution
+from simmons_azzopardi_class import SASolution
 from ct_class import CTSolution
 #from ct import beta as CTbeta
---
+++ pyfd/pbe/moc/simmons_azzopardi_class.py
@@ -0,0 +1,51 @@
+from case_class import CaseSolution
+from numpy import zeros, pi
+
+
+class SASolution(CaseSolution):
+    def __init__(
+            self,
+            M=10,
+            U=2.71,  # [rps] impeller revolutions
+            phi=0.117,  # [1] holdup
+            v0=4e-11,  # [cm^3]
+            model_parameters=None):
+
+        # pipe diamter andlength
+        self.D = 0.063  # [m] impeller diameter
+        self.L = 4.5    # [m] impeller diameter
+
+        contProperties = dict()
+        dispProperties = dict()
+        domainProperties = dict()
+
+        # oil
+        contProperties['mu'] = 1.8e-3  # [P = kg * m^-1 s^-1]
+        contProperties['rho'] = 797.  # [kg/cm3]
+
+        # calculate turbulent properties
+        Re = U * self.L / contProperties['mu'] * contProperties['rho']
+        I = 0.16 * Re ** (1. / 8.)
+        u_rms = U * I
+        k = 3. / 2. * u_rms ** 2
+        L_t = 0.038 * self.D
+        contProperties['epsilon'] = 0.09 * k ** (3./2.) / L_t
+        # water solution
+        dispProperties['sigma'] = 1.e-2  # [P = kg * m^-1 s^-1]
+        dispProperties['rho'] = 1166.  # [kg/m3]
+        dispProperties['phi'] = phi
+
+        dispProperties['vMax'] = 6e-11
+
+        # Feed distribution
+        dispProperties['v0'] = v0
+        dispProperties['sigma0'] = v0 / 10
+
+        # Feed
+        domainProperties['theta'] = 600.
+        domainProperties['V'] = pi * self.L * (self.D / 2) ** 2
+        domainProperties['M'] = M
+
+        CaseSolution.__init__(
+            self, dispProperties, contProperties, domainProperties,
+            model_parameters=model_parameters)
