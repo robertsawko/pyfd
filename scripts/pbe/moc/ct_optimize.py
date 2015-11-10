3e05265ca38d33f99cccc7c0d9e0dfa16a35a2b8
s200292 <marcin.traczyk.88@gmail.com>
2015-11-10 10:54:03 +0000

    ADD C&T case optimization results

--- scripts/pbe/moc/ct_optimize.py
+++ scripts/pbe/moc/ct_optimize.py
@@ -11,18 +11,6 @@ class ct_experiment:
         self.Nstar = Nstar
         self.d32 = d32
 
-concentrations = [5, 10, 15]
-
-mm_to_m = 0.001
-experiments = []
-for c in concentrations:
-    data = genfromtxt(
-        '../../../pyfd/data/pbe/coulaloglou/d32_N_alpha{0}.txt'.format(c))
-    for i in range(data.shape[0]):
-        experiments.append(
-            ct_experiment(c / 100., data[i, 0] / 60, data[i, 1] * mm_to_m))
-
-
 def error_function(C, experiment):
     v0s = array([0.5, 1.5]) * pi / 6 * experiment.d32**3
     mp = array(C)
@@ -38,27 +26,30 @@ def error_function(C, experiment):
     print('constants={0}, error={1}'.format(C, error))
     return error
 
+concentrations = [5, 10, 15]
+mm_to_m = 0.001
+
+experiments = []
+for c in concentrations:
+    data = genfromtxt(
+        '../../../pyfd/data/pbe/coulaloglou/d32_N_alpha{0}.txt'.format(c))
+    for i in range(data.shape[0]):
+        experiments.append(
+            ct_experiment(c / 100., data[i, 0] / 60, data[i, 1] * mm_to_m))
+
+
 c0 = [0.4, 0.08, 2.8, 1.83]  # CT original constants
-best_fit_constants = []
+results = []
 for e in experiments[0:1]:
-    # do our magic optimization
-    #print(
-        #'Optimizing phi={0:0.2f}, Nstar={1:0.2f}'.format(e.phi, e.Nstar),
-        #end='...', flush=True)
+    res = dict()
+
     Copt = minimize(
         lambda c: error_function(c, e), c0,
         method='TNC', bounds=[(0, None)] * 4,
-        options={'disp': False, 'ftol': 0.01})
-    #if Copt.success:
-        #print('success.')
-    #else:
-        #print('failed.')
-    best_fit_constants.append(Copt.x)
+        options={'disp': False, 'ftol': 0.01, 'maxiter': 1})
+    res['best_fit'] = Copt.x
+    res['setup'] = {'phi': e.phi, 'Nstar': e.Nstar, 'd32': e.d32}
+    results.append(res)
 
-#for b in best_fit_constants:
-    #print b
-
-with open('data/ct_optimization_cases.pickle', 'wb') as f:
-    pickle.dump(experiments, f)
-with open('data/ct_optimization_parameters.pickle', 'wb') as f:
-    pickle.dump(best_fit_constants, f)
+with open('data/ct_optimization_results.pickle', 'wb') as f:
+    pickle.dump(results, f)
