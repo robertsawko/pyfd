d1812197c6f57b175c68c226003de0285a8f7ae4
Robert Sawko <r.sawko@cranfield.ac.uk>
2015-11-13 17:11:11 +0000

    Correct the __init__ file for moc

--- pyfd/pbe/moc/__init__.py
+++ pyfd/pbe/moc/__init__.py
@@ -1,6 +1,1 @@
-from moc import MOCSolution
-from case_class import beta, CaseSolution
-from simmons_azzopardi_class import SASolution
-from angeli_class import AngeliSolution
-from ct_class import CTSolution
-#from ct import beta as CTbeta
+from pbe.moc import MOCSolution
