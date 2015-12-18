857fcd4de01b9b871b47853a34440b9512b59a97
Robert Sawko <r.sawko@cranfield.ac.uk>
2015-12-18 16:12:02 +0000

    Tidy up some package references

--- scripts/pbe/moc/ct_calculate.py
+++ scripts/pbe/moc/ct_calculate.py
@@ -9,8 +9,8 @@ figure 3 from CT publication.
 concentrations = [5]  # , 10, 15]
 Ns = linspace(3, 6, 10)  # rps
 ct_solutions = dict([(
-    c, [CTSolution(M=50, Nstar=N, phi=c / 100.0) for N in Ns])
+    c, [CTSolution(M=200, Nstar=N, phi=c / 100.0) for N in Ns])
     for c in concentrations])
 
-with open('data/ct_solutions.pickle', 'wb') as f:
+with open('ct_solutions.pickle', 'wb') as f:
     pickle.dump(ct_solutions, f)
